cd68bdcb4c5d3134c74989eb60f223ca510940e1
SuhanovaE <98471539+SuhanovaE@users.noreply.github.com>
2022-05-04 10:07:26 +0300

    Add files via upload

---
+++ Суханова16/Суханова16/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Суханова16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Практическая работа №16");
+            Console.WriteLine("Выполнила: студентка группы ИСП.20А");
+            Console.WriteLine("Суханова Екатерина");
+            Console.WriteLine("Вариант №3");
+            Console.WriteLine();
+            Console.WriteLine("Введите количество студентов:");
+            int N = int.Parse(Console.ReadLine());
+            STUDENT[] student = new STUDENT[N];
+            StreamReader sr = new StreamReader(@"Input.txt");
+            for (int i = 0; i < N; i++)
+            {
+                student[i] = new STUDENT();
+                student[i].InputFromFile(sr);               
+                Console.WriteLine(student[i].Output());
+            }
+            Console.WriteLine();
+            bool flag = false;
+            Console.WriteLine("Студенты, у которых есть хотя бы одна 2");
+            for (int i = 0; i < N; i++)
+            {
+                if (student[i].English == 2 || student[i].Math == 2 || student[i].Biology == 2 || student[i].Chemistry == 2 || student[i].Physics == 2 )
+                {
+                    Console.WriteLine(student[i].PrintInfo());
+                    student[i].Save();
+                    flag = true;
+
+                }
+                
+
+            }
+            if (flag==false)
+                Console.WriteLine("нет студентов, у которых есть 2");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Суханова16/Суханова16/STUDENT.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Суханова16
+{
+    class STUDENT
+    {
+        private string fio;
+        private string group;
+        private int math;
+        private int english;
+        private int physics;
+        private int biology;
+        private int chemistry;
+
+        public string FIO
+        {
+            get { return fio; }
+            set { fio = value; }
+        }
+        public string Group
+        {
+            get { return group; }
+            set { group = value; }
+        }
+        public int Physics
+        {
+            get { return physics; }
+            set { physics = value; }
+        }
+        public int Biology
+        {
+            get { return biology; }
+            set { biology = value; }
+        }
+        public int Math
+        {
+            get { return math; }
+            set { math = value; }
+        }
+        public int English
+        {
+            get { return english; }
+            set { english = value; }
+        }
+        public int Chemistry
+        {
+            get { return chemistry; }
+            set { chemistry = value; }
+        }
+
+        public STUDENT()
+        {
+            FIO = "Иванов И.И.";
+            group = "ИСП.20";
+            math = 4;
+            english = 5;
+            physics = 3;
+            biology = 5;
+            chemistry = 5;
+        }
+        public STUDENT(string fio, string gr, int m, int eng, int ph, int bio, int ch)
+        {
+            FIO = fio;
+            group = gr;
+            math = m;
+            english = eng;
+            physics = ph;
+            biology = bio;
+            chemistry = ch;
+        }
+        public void InputFromFile(StreamReader sr)
+        {
+
+            string input = sr.ReadLine();
+            string[] info = input.Split(',');
+            FIO = info[0];
+            group = info[1];
+            math = int.Parse(info[2]);
+            english = int.Parse(info[3]);
+            biology = int.Parse(info[4]);
+            chemistry = int.Parse(info[5]);
+            physics = int.Parse(info[6]);
+
+        }
+        public string Output()
+        {
+            return $"Студент {FIO} " +
+                $"группа {group} \n" +
+                $"Оценка по математике {math} \n" +
+                $"Оценка по английскому языку {english} \n" +
+                $"Оценка по физике {physics} \n" +
+                $"Оценка по химии {chemistry} \n" +
+                $"Оценка по биологии {biology} ";
+        }
+        public string PrintInfo()
+        {
+            return $"Студент {FIO} " +
+                $"группа {group} ";
+        }
+        public void Save()
+        {
+            StreamWriter sr = new StreamWriter(@"Result.txt", true);
+            sr.WriteLine($"{FIO},{group}");
+            sr.Close();
+        }
+        
+    }
+    
+}
+
